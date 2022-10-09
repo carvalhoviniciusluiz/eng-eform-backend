3d2840c3067a0f3ee1795c049ebc3c107a155155
Vinicius Carvalho <vinicius.carvalho@houpa.app>
2022-10-09 10:02:12 -0300

    refactor: add order by to questions list

--- src/root/root.service.ts
+++ src/root/root.service.ts
@@ -14,6 +14,7 @@ export class RootService {
           where: {
             parentId: null
           },
+          orderBy: { createdAt: 'asc' },
           include: {
             answers: true,
             children: {
