2ac90aae85ac7b6f782bd7ff0b716ece70a3998a
Vinicius Carvalho <vinicius.carvalho@houpa.app>
2022-10-09 13:31:17 -0300

    refactor: add default response to get form by id

---
+++ src/forms/dtos/form-with-questions.response.dto.ts
@@ -0,0 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Form as FormModel, Question as QuestionModel } from '@prisma/client';
+
+type Params = {
+  form: FormModel;
+  rows: QuestionModel[];
+};
+
+export class FormWithQuestionsResponseDto {
+  @ApiProperty({
+    example: [
+      {
+        id: '64aedc98-884a-4d31-a535-bf893e60284e',
+        content: 'Question',
+        updatedAt: '2022-05-25T10:31:43.279Z'
+      }
+    ]
+  })
+  data: any;
+
+  @ApiProperty({
+    example: {
+      id: 'fa1d2eee-d158-42e8-bb71-4e7c92c27c34',
+      name: 'Associate',
+      updatedAt: '2022-05-23T12:49:45.655Z'
+    }
+  })
+  form: any;
+
+  constructor(params: Params) {
+    const { form, rows } = params;
+
+    this.form = {
+      id: form.id,
+      name: form.name,
+      status: form.status,
+      updatedAt: form.updatedAt
+    };
+
+    this.data = rows.map(question => ({
+      id: question.id,
+      content: question.content,
+      updatedAt: question.updatedAt
+    }));
+  }
+}
--- src/forms/dtos/index.ts
+++ src/forms/dtos/index.ts
@@ -1,3 +1,4 @@
 export * from './form-paginate.dto';
 export * from './form-paginate.response.dto';
 export * from './form-request.dto';
+export * from './form-with-questions.response.dto';
