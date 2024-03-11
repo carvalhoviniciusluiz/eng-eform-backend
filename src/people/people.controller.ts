27fd1bded425afebb184e86e15874acc945223b4
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2024-03-11 15:09:40 -0300

    fix: param resolved

--- src/people/people.controller.ts
+++ src/people/people.controller.ts
@@ -23,62 +23,12 @@ export class PeopleController {
   }
 
   @Get('/:name')
-  async getPerson(@Param(':name') name: string) {
+  async getPerson(@Param('name') name: string) {
     try {
-      const response = await this.peopleService.getPerson(name);
-      const output = response.map(res => {
-        return {
-          person: {
-            id: res.id,
-            name: res.name,
-            socialName: res.socialName,
-            birthDate: res.birthDate
-          },
-          adresses: res.adresses.map((address: any) => ({
-            id: address.id,
-            number: address.number,
-            zipCode: address.zipCode,
-            publicPlace: address.publicPlace,
-            neighborhood: address.neighborhood,
-            neighborhoodComplement: address.neighborhoodComplement,
-            county: address.county,
-            city: address.city
-          })),
-          documents: res.documents.map(document => ({
-            id: document.id,
-            documentType: document.documentType,
-            documentNumber: document.documentNumber,
-            shippingDate: document.shippingDate
-          })),
-          contacts: res.contacts.map(contact => ({
-            id: contact.id,
-            contactType: contact.contactType,
-            contact: contact.contact
-          })),
-          questions: res.personalData.map(data => {
-            const hasResponse = !!data.questionAnswer.response;
-            const hasAnswer = !!data.questionAnswer.answer;
-            let content: any = {};
-            if (hasAnswer && hasResponse) {
-              content[data.questionAnswer.answer.id] = data.questionAnswer.response;
-            } else if (hasAnswer && !hasResponse) {
-              content = data.questionAnswer.answer.id;
-            } else if (hasResponse) {
-              content['response'] = data.questionAnswer.response;
-            }
-            return {
-              [data.questionAnswer.question.id]: content,
-              metadata: {
-                personQuestionAnswerId: data.id,
-                questionAnswerId: data.questionAnswer.id
-              }
-            };
-          })
-        };
-      });
+      const output = await this.peopleService.getPerson(name);
       return output;
     } catch (error) {
-      throw new BadRequestException();
+      throw new BadRequestException(error.message);
     }
   }
 }
--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -51,6 +51,56 @@ export class PeopleService {
         }
       }
     });
-    return people;
+    const output = people.map(res => {
+      return {
+        person: {
+          id: res.id,
+          name: res.name,
+          socialName: res.socialName,
+          birthDate: res.birthDate
+        },
+        adresses: res.adresses.map((address: any) => ({
+          id: address.id,
+          number: address.number,
+          zipCode: address.zipCode,
+          publicPlace: address.publicPlace,
+          neighborhood: address.neighborhood,
+          neighborhoodComplement: address.neighborhoodComplement,
+          county: address.county,
+          city: address.city
+        })),
+        documents: res.documents.map(document => ({
+          id: document.id,
+          documentType: document.documentType,
+          documentNumber: document.documentNumber,
+          shippingDate: document.shippingDate
+        })),
+        contacts: res.contacts.map(contact => ({
+          id: contact.id,
+          contactType: contact.contactType,
+          contact: contact.contact
+        })),
+        questions: res.personalData.map(data => {
+          const hasResponse = !!data.questionAnswer.response;
+          const hasAnswer = !!data.questionAnswer.answer;
+          let content: any = {};
+          if (hasAnswer && hasResponse) {
+            content[data.questionAnswer.answer.id] = data.questionAnswer.response;
+          } else if (hasAnswer && !hasResponse) {
+            content = data.questionAnswer.answer.id;
+          } else if (hasResponse) {
+            content['response'] = data.questionAnswer.response;
+          }
+          return {
+            [data.questionAnswer.question.id]: content,
+            metadata: {
+              personQuestionAnswerId: data.id,
+              questionAnswerId: data.questionAnswer.id
+            }
+          };
+        })
+      };
+    });
+    return output;
   }
 }
